3ddd002c5da8bb3ed6718b05d7f8830fc013ac57
Ashbel <kashbel747@gmail.com>
2025-07-07 08:46:26 +0300

    Configure GitHub Pages deployment

--- practising-react/vite.config.js
+++ practising-react/vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace with your repo name
+const repoName = 'first-react-practice'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`, // 👈 very important
 })
